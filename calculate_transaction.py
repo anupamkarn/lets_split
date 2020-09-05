49ee5c88a6ec25ada99a5aacb54ac871051bfabd
Anupam Karn <anupamkarn@Anupams-MacBook-Pro.local>
2020-09-05 21:44:13 +0530

    graph implementation completed

--- calculate_transaction.py
+++ calculate_transaction.py
@@ -1,5 +1,18 @@
 
 def optimize_transaction(transaction_list):
 
-    return transaction_list
+    transactions = transaction_list['transaction_list']
+
+    graph = {}
+
+    #Adjacentcy list implementation 
+    for transaction in transactions:
+        if transaction[0] not in graph:
+            graph[transaction[0]]=[]
+            graph[transaction[0]].append([transaction[1],transaction[2]]) 
+        else:
+            graph[transaction[0]].append([transaction[1],transaction[2]])
+
+        
+
     
